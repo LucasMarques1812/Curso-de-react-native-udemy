2b4229a648a3e917c6daa6c71e4b57560e627f3b
Lucas Marques <Colten610@gmail.com>
2023-03-14 20:25:06 -0300

    Avanço 1

---
+++ App.js
@@ -0,0 +1,29 @@
+import React, { Component } from "react";
+import { View, Text } from "react-native";
+
+export default function App(){
+    return(
+        <View>
+            <Text>Olá mundo!</Text>
+            <Text>Meu primeiro App</Text>
+        </View>
+    );
+}
+
+
+
+// class App extends Component{
+//     render(){
+//         return(
+//             <View>
+//                 <Text>Olá mundo de classes!</Text>
+//                 <Text>Meu primeiro App</Text>
+//             </View>
+//         );
+//     }
+// }
+
+// export default App;
+
+
+
